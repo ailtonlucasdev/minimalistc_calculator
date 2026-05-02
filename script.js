ea5234d81c7100ceb4105f200293a8efd144e69f
ailtonlucadev <ailtonlucasdev@gmail.com>
2026-05-02 12:34:30 -0300

    iniciando a função de calcular - não estilizada

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function back() {
     const operations = document.getElementById('operations');
     operations.value = operations.value.slice(0, -1);
 }
+
+function calculate() {
+    const calculateOperation = document.getElementById('operations');
+    if (calculateOperation) {
+        operations.value = eval(calculateOperation.value)
+    }
+}
