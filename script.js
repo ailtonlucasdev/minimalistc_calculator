728c710b6f53898350cc1e6d10be8e87a710f9f4
ailtonlucadev <ailtonlucasdev@gmail.com>
2026-05-02 14:45:13 -0300

    ajustando o operadores matemáticos

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ function back() {
 
 function calculate() {
     const calculateOperation = document.getElementById('operations');
-    if (calculateOperation) {
-        operations.value = eval(calculateOperation.value)
+    if (calculateOperation && calculateOperation.value) {
+        let expression = calculateOperation.value;
+        // tratamento com expressões regex
+        expression = expression.replace(/x²/g, '**2')  // potenciação
+        expression = expression.replace(/÷/g, '/')  // divisão 
+        expression = expression.replace(/x/g, '*')  // multiplicação
+        expression = expression.replace(/%/g, '/100')  // porcentagem
+        expression = expression.replace(/√(\d+(\.\d+)?)/g, 'Math.sqrt($1)')  // radiciação
+
+        try {
+            calculateOperation.value = eval(expression);
+        }
+        catch (e) {
+            calculateOperation.value = "Operação inválida"
+        }
     }
-    
+
 }
